7625a555d263b452d483a6c0c345aef0a9038c35
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-06 16:42:50 +0500

    first commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
     mysql: {
@@ -24,12 +24,12 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        connectionString: env('postgresql://postgres.ttswlbkwtibxqsxhbnho:ff1987635690@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres'),
+        host: env('DATABASE_HOST', 'aws-0-ap-southeast-1.pooler.supabase.com'),
+        port: env.int('DATABASE_PORT', 6543),
+        database: env('DATABASE_NAME', 'postgres'),
+        user: env('DATABASE_USERNAME', 'postgres.ttswlbkwtibxqsxhbnho'),
+        password: env('DATABASE_PASSWORD', 'ff1987635690'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
